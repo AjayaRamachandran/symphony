2aaf94f945fb13ed8826562eb14123344f138194
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2025-07-10 17:28:37 -0400

    added drag drop, added stars to file items

---
+++ src/modals/InvalidDrop.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function InvalidDrop({onComplete}) {
+
+  return (
+    <>
+      <div className='modal-title' text-style='display' style={{marginBottom : '15px'}}>Invalid Drop</div>
+      <div className='modal-paragraph'>Symphony only allows drag-and-drop for .symphony files.</div>
+      <button className={'call-to-action-2'} text-style='display' onClick={() => onComplete()}>Okay</button>
+    </>
+  );
+}
+
+export default InvalidDrop;
