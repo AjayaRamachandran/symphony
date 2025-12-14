46c4dc21c675373661b01364febb133ab335e4fe
Ajaya Ramachandran <ramachandran.aj@northeastern.edu>
2025-12-14 06:06:15 -0500

    fixed small issue with custom.py

--- inner/src/gui/custom.py
+++ inner/src/gui/custom.py
@@ -18,8 +18,8 @@ import gui.element as gui
 
 def getColorStates(width, height, source_path):
     rainbowImage = pygame.image.load(f"{source_path}/assets/rainbow.png")
-    colors = [(237, 144, 68, 255),
-            (158, 86, 207, 255),
+    colors = [(235, 144, 74, 255),
+            (184, 125, 227, 255),
             (108, 207, 198, 255),
             (154, 222, 138, 255),
             (106, 122, 214, 255),
@@ -27,8 +27,8 @@ def getColorStates(width, height, source_path):
     outputs = []
     for idx, color in enumerate(colors):
         surf = pygame.Surface((width, height), pygame.SRCALPHA)
-        surf.fill(color)
-        gui.stamp(surf, str(idx), gui.SUBHEADING1, width/2, height/2, gui.BG_COLOR, justification='center')
+        pygame.draw.rect(surf, color, (0, 0, width, height), border_radius=3)
+        gui.stamp(surf, str(idx + 1), gui.SUBHEADING1, width/2, height/2, gui.BG_COLOR, justification='center')
         outputs.append(surf)
     
     gui.stamp(rainbowImage, '7', gui.SUBHEADING1, rainbowImage.get_width()/2, rainbowImage.get_height()/2, gui.BG_COLOR, justification='center')
